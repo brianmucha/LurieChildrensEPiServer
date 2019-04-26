a59ca007a1bb43b20d1749777fe0b21ab54d1187
Brian Mucha <BMucha@luriechildrens.org>
2019-04-26 12:29:33 -0500

    Mostly updates to Bootstrap nad Jquery.

---
+++ LurieChildrensFoundation._Base/Models/PropertyTypes/LogoType.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer;
+using EPiServer.Core;
+using EPiServer.DataAnnotations;
+using EPiServer.Shell.ObjectEditing;
+using EPiServer.Web;
+
+using LurieChildrensFoundation._Base.Models.Blocks;
+
+namespace LurieChildrensFoundation._Base.Models.PropertyTypes
+{
+	/// <summary>
+	/// This Property Type is used on the Start Page to define the site logo.
+	/// </summary>
+	public class LogoType : FndBaseBlock
+	{
+		[CultureSpecific]
+		public virtual String Title { get; set; }
+
+		[DefaultDragAndDropTarget]
+		[UIHint(UIHint.Image)]
+		public virtual Url Url { get; set; }
+	}
+}
