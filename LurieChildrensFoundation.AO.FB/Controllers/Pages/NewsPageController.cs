aa275da930cf8f8fde60bddcb937d2842603256f
Brian Mucha <BMucha@luriechildrens.org>
2019-05-08 08:36:45 -0500

    News Page Fixes

---
+++ LurieChildrensFoundation.AO.FB/Controllers/Pages/NewsPageController.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+using LurieChildrensFoundation.AO._Base.Controllers.Pages;
+using LurieChildrensFoundation.AO.FB.Models.Pages;
+using LurieChildrensFoundation.AO.FB.Models.ViewModels;
+
+namespace LurieChildrensFoundation.AO.FB.Controllers
+{
+	/// <summary>
+	/// This Page Controller provides the implementation for a <see cref="NewsPage"/>.
+	/// </summary>
+	public class NewsPageController : AONewsPageController<NewsPage>
+	{
+		public ActionResult Index(NewsPage currentPage)
+		{
+			var viewModel = NewsPageViewModel.Create(currentPage);
+
+			// Populate any additional properties in the ViewModel that are not part of the Model.
+			viewModel.ViewModelProperty = "This is a ViewModel property populated by the controller.";
+
+			return View(viewModel);
+		}
+	}
+}
---
+++ LurieChildrensFoundation.AO.FB/Models/ViewModels/NewsPageViewModel.cs
@@ -0,0 +1,35 @@
+﻿using LurieChildrensFoundation.AO._Base.Models.ViewModels;
+using LurieChildrensFoundation.AO.FB.Models.Pages;
+
+namespace LurieChildrensFoundation.AO.FB.Models.ViewModels
+{
+	/// <summary>
+	/// Returns a Page View Model for a <see cref="NewsPage"/>.
+	/// </summary>
+	public class NewsPageViewModel : AONewsPageViewModel
+	{
+		/// <summary>
+		/// Returns a PageViewModel of type <typeparam name="T"/>.
+		/// </summary>
+		/// <remarks>
+		/// Convenient method for creating PageViewModels without having to specify the type as methods can use type inference while constructors cannot.
+		/// </remarks>
+		new public static NewsPageViewModel<T> Create<T>(T page) where T : NewsPage
+		{
+			return new NewsPageViewModel<T>(page);
+		}
+	}
+
+	/// <summary>
+	/// Defines a Page View Model for a <see cref="NewsPage"/>.
+	/// </summary>
+	public class NewsPageViewModel<T> : AONewsPageViewModel<T> where T : NewsPage
+	{
+		public NewsPageViewModel(T currentPage) : base(currentPage)
+		{
+			CurrentPage = currentPage;
+		}
+
+		new public T CurrentPage { get; private set; }
+	}
+}
